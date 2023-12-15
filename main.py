0d6f715884109413272c28ec2c22841d893bacd2
PrincessNorn <princessnorn@gmail.com>
2023-12-15 14:49:04 -0500

    Adding requirements.txt

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+import math
+
 from bs4 import BeautifulSoup as bs
 import lxml
+import re
 import requests
 import streamlit as st
+
+def get_irc(irc, key):
+    soup = bs(requests.get("https://www.law.cornell.edu/uscode/text/26/" + irc.upper()).text, "lxml")
+    body = soup.find("div", class_="section")
+    for a in body.findAll('a', href=True):
+        a.href = "https://www.law.cornell.edu" + str(a.href)
+    sections = body.findAll(text=re.compile("section.*\d+"))
+    st.markdown(body, unsafe_allow_html=True)
+    for codes in sections:
+        text = codes.getText()
+        numbers = re.split(" |,", re.search("section.*(\d+)", text).group())
+        for number in numbers:
+            if re.search("\d", number):
+                st.write("")
+                st.write("")
+                show_irc = st.checkbox("[Section " + str(number) + "](https://www.law.cornell.edu/uscode/text/26/" + str(number) + ")", key=str(key)+" "+number)
+                key += 1
+                if show_irc:
+                    get_irc(number, key)
 
 st.set_page_config("IRC_Lookup", None, "wide")
 
 st.title("IRC Lookup")
-irc = st.text_input("IRC")
+irc_input = st.text_input("IRC")
 
-if irc:
-    soup = bs(requests.get("https://www.law.cornell.edu/uscode/text/26/"+irc.upper()).text, "lxml")
-    soup = soup.find("div", class_="section")
-    for a in soup.findAll('a', href=True):
-        a['href'] = "https://www.law.cornell.edu"+str(a['href'])
-    st.markdown(soup, unsafe_allow_html=True)
-
+if irc_input:
+    get_irc(irc_input, 0)
